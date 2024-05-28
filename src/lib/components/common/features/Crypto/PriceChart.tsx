0569d10f4e99c8b77179a60b02b988f0160a20b2
emad <emadi68moh@gmail.com>
2024-05-29 01:12:16 +0330

    feat: connecting to API

--- src/lib/components/common/features/Crypto/PriceChart.tsx
+++ src/lib/components/common/features/Crypto/PriceChart.tsx
@@ -1,11 +1,20 @@
+import { useState, useEffect } from 'react';
+import { useGet } from '../../../../util/API/hooks';
+
 // ── components
 import { Chart } from '../../chart/Chart';
+import { BarGroup } from '../../chart/BarGroup';
+import { Bar } from '../../chart/Bar';
 
 // ── types
 import type { FC } from 'react';
 import type { TChartLevel } from '../../chart/Chart';
-import { BarGroup } from '../../chart/BarGroup';
-import { Bar } from '../../chart/Bar';
+import type { IHourlyPairOHLCV, IHourlyPairOHLCVResponse } from '../../../../util/types/market';
+
+interface IMinMax {
+  min: number;
+  max: number;
+}
 
 // ── constants
 const LEVELS: TChartLevel = [
@@ -26,60 +35,85 @@ const LEVELS: TChartLevel = [
   },
 ];
 
+// ── constants
+const CACHE_CONFIG = { address: 'smartpath-ohlcv', revalidationTime: 60 * 60 * 1000 };
+
 //          ╭─────────────────────────────────────────────────────────╮
 //          │                       components                        │
 //          ╰─────────────────────────────────────────────────────────╯
-
 export const PriceChart: FC = () => {
+  const [minMax, setMinMax] = useState<IMinMax>();
+
+  const { data, fetch } = useGet<IHourlyPairOHLCVResponse>({
+    url: 'https://min-api.cryptocompare.com/data/v2/histohour',
+    withCache: true,
+    cacheConfig: CACHE_CONFIG,
+  });
+
+  useEffect(() => {
+    fetch({
+      params: {
+        fsym: 'BTC',
+        tsym: 'USD',
+        limit: '10',
+        api_key: '365694a330b56587f1e2a5b53a1d24c68ecbfbb1dadfee33c5848e299c896ae8',
+      },
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log('----', data?.Data);
+    if (data) {
+      //setSelectedVolume(data.Data[0]);
+      const clone = [...data.Data.Data];
+      let _min = clone.sort((a, b) => b.high - a.high)[0].high;
+      const _max = clone.sort((a, b) => a.low - b.low)[0].low;
+
+      const amountToReduceMin = (_max - _min) / 10;
+      _min = _min - amountToReduceMin;
+
+      setMinMax({ min: _min, max: _max });
+    }
+  }, [data]);
+
+  console.log({ minMax });
+
   return (
     <div className='h-full flex flex-col gap-2'>
-      <div className='text-center'>
-        <h4 className='text-lg font-bold'>Market Value of</h4>
-      </div>
       <Chart levels={LEVELS}>
-        <BarGroup className='mx-2' title='value 1'>
-          <Bar min={0} max={100} size='md' value={30} color='bg-custom-green-400' />
-          <Bar min={0} max={100} size='md' value={50} color='bg-custom-golden-400' />
-          <Bar min={0} max={100} size='md' value={80} color='bg-custom-red-400' />
-        </BarGroup>
-        <BarGroup className='mx-2' title='value 2'>
-          <Bar min={0} max={100} size='md' value={20} color='bg-custom-green-400' />
-          <Bar min={0} max={100} size='md' value={30} color='bg-custom-golden-400' />
-          <Bar min={0} max={100} size='md' value={90} color='bg-custom-red-400' />
-        </BarGroup>
-
-        <BarGroup className='mx-2' title='value 1'>
-          <Bar min={0} max={100} size='md' value={30} color='bg-custom-green-400' />
-          <Bar min={0} max={100} size='md' value={50} color='bg-custom-golden-400' />
-          <Bar min={0} max={100} size='md' value={80} color='bg-custom-red-400' />
-        </BarGroup>
-        <BarGroup className='mx-2' title='value 2'>
-          <Bar min={0} max={100} size='md' value={20} color='bg-custom-green-400' />
-          <Bar min={0} max={100} size='md' value={30} color='bg-custom-golden-400' />
-          <Bar min={0} max={100} size='md' value={90} color='bg-custom-red-400' />
-        </BarGroup>
-
-        <BarGroup className='mx-2' title='value 1'>
-          <Bar min={0} max={100} size='md' value={30} color='bg-custom-green-400' />
-          <Bar min={0} max={100} size='md' value={50} color='bg-custom-golden-400' />
-          <Bar min={0} max={100} size='md' value={80} color='bg-custom-red-400' />
-        </BarGroup>
-        <BarGroup className='mx-2' title='value 2'>
-          <Bar min={0} max={100} size='md' value={20} color='bg-custom-green-400' />
-          <Bar min={0} max={100} size='md' value={30} color='bg-custom-golden-400' />
-          <Bar min={0} max={100} size='md' value={90} color='bg-custom-red-400' />
-        </BarGroup>
-
-        <BarGroup className='mx-2' title='value 1'>
-          <Bar min={0} max={100} size='md' value={30} color='bg-custom-green-400' />
-          <Bar min={0} max={100} size='md' value={50} color='bg-custom-golden-400' />
-          <Bar min={0} max={100} size='md' value={80} color='bg-custom-red-400' />
-        </BarGroup>
-        <BarGroup className='mx-2' title='value 2'>
-          <Bar min={0} max={100} size='md' value={20} color='bg-custom-green-400' />
-          <Bar min={0} max={100} size='md' value={30} color='bg-custom-golden-400' />
-          <Bar min={0} max={100} size='md' value={90} color='bg-custom-red-400' />
-        </BarGroup>
+        {minMax && data
+          ? data.Data.Data.map((item, index) => {
+              return (
+                <BarGroup
+                  key={index}
+                  className='mx-1'
+                  title={String(new Date(item.time * 1000).getHours())}
+                >
+                  <Bar
+                    min={minMax.min}
+                    max={minMax.max}
+                    size='sm'
+                    value={item.high}
+                    color='bg-custom-green-500'
+                  />
+                  <Bar
+                    min={minMax.min}
+                    max={minMax.max}
+                    size='sm'
+                    value={item.open}
+                    color='bg-custom-golden-500'
+                  />
+                  <Bar
+                    min={minMax.min}
+                    max={minMax.max}
+                    size='sm'
+                    value={item.low}
+                    color='bg-custom-red-500'
+                  />
+                </BarGroup>
+              );
+            })
+          : null}
       </Chart>
     </div>
   );
