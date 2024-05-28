3ff701fa013aeb9a3031a7832d5cdb67f19fc1de
emad <emadi68moh@gmail.com>
2024-05-28 20:26:19 +0330

    fix: removing dates state

--- src/lib/components/common/features/Crypto/CryptoChart.tsx
+++ src/lib/components/common/features/Crypto/CryptoChart.tsx
@@ -1,5 +1,3 @@
-import { useState, useEffect } from 'react';
-
 // ── components
 import { MarketValue } from './MarketValue';
 import { PriceChart } from './PriceChart';
@@ -10,28 +8,7 @@ import type { FC } from 'react';
 //          ╭─────────────────────────────────────────────────────────╮
 //          │                        component                        │
 //          ╰─────────────────────────────────────────────────────────╯
-
 export const CryptoChart: FC = () => {
-  const [dates, setDates] = useState<Date[]>();
-
-  useEffect(() => {
-    const newDates: Date[] = [];
-    const today = new Date();
-    newDates.push(today);
-    /**
-     * count days down to create an array for the last 10 days
-     */
-    for (let i = 1; i < 10; i++) {
-      const newDate = new Date(today);
-      newDate.setDate(newDate.getDate() - i);
-      newDates.push(newDate);
-    }
-
-    console.log(newDates);
-
-    setDates(newDates);
-  }, []);
-
   return (
     <div className='grid grid-rows-2 w-full m-12 gap-4 p-4 rounded-xl bg-[#f5f6f8]'>
       <div className='grid grid-cols-12 w-full gap-4'>
@@ -39,7 +16,7 @@ export const CryptoChart: FC = () => {
           <PriceChart />
         </div>
         <div className='col-span-4 rounded-xl bg-white p-4 min-h-96 shadow-md shadow-gray-200'>
-          <MarketValue dates={dates} />
+          <MarketValue />
         </div>
       </div>
       <div className='bg-white p-4 shadow-md shadow-gray-200 rounded-xl flex'>
