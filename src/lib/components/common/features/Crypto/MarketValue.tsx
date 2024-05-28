2a62ae3a528307540043396288400de929a67a2c
emad <emadi68moh@gmail.com>
2024-05-28 18:38:13 +0330

    feat: added props date and created checkbox for each date

--- src/lib/components/common/features/Crypto/MarketValue.tsx
+++ src/lib/components/common/features/Crypto/MarketValue.tsx
@@ -1,10 +1,15 @@
 // ── components
 import { Chart } from '../../chart/Chart';
 import { Bar } from '../../chart/Bar';
+import { Checkbox } from '../../checkbox/Checkbox';
 
 // ── types
 import type { FC } from 'react';
 import type { TChartLevel } from '../../chart/Chart';
+
+interface IMarketValueProps {
+  dates?: Date[];
+}
 
 // ── constants
 const LEVELS: TChartLevel = [
@@ -26,15 +31,24 @@ const LEVELS: TChartLevel = [
 //          │                       components                        │
 //          ╰─────────────────────────────────────────────────────────╯
 
-export const MarketValue: FC = () => {
+export const MarketValue: FC<IMarketValueProps> = ({ dates }) => {
   return (
     <div className='h-full flex flex-col gap-2'>
       <div className='text-center'>
         <h4 className='text-lg font-bold'>Market Value of</h4>
       </div>
-      <Chart levels={LEVELS}>
-        <Bar min={0} max={100} value={65} size='lg' color='bg-custom-green-400' />
-      </Chart>
+      <div className='w-full h-full pb-4 flex gap-4 flex-col'>
+        <Chart levels={LEVELS}>
+          <Bar min={0} max={100} value={65} size='lg' color='bg-custom-green-400' />
+        </Chart>
+        <div className='flex justify-center gap-2'>
+          {dates
+            ? dates.map((_, index) => {
+                return <Checkbox key={index} />;
+              })
+            : null}
+        </div>
+      </div>
     </div>
   );
 };
