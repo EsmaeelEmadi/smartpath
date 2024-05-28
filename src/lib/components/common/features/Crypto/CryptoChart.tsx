fe580adf34b7e65837aa9347fb92d36b7d1072cc
emad <emadi68moh@gmail.com>
2024-05-29 02:08:18 +0330

    feat: creating indexes based on props

--- src/lib/components/common/features/Crypto/CryptoChart.tsx
+++ src/lib/components/common/features/Crypto/CryptoChart.tsx
@@ -1,17 +1,53 @@
+import { useState } from 'react';
+
 // ── components
 import { MarketVolume } from './MarketVolume';
 import { PriceChart } from './PriceChart';
-import { IndexController } from './IndexController';
+import { IIndexController, IndexController } from './IndexController';
 
 // ── types
 import type { FC } from 'react';
 
-//interface selected
+// ── constants
+const INDEXES: Readonly<IIndexController[]> = [
+  {
+    key: 'high',
+    title: 'Higher',
+    color: 'success',
+    checked: true,
+  },
+  {
+    key: 'open',
+    title: 'Average',
+    color: 'warning',
+    checked: true,
+  },
+  {
+    key: 'low',
+    title: 'Lower',
+    color: 'danger',
+    checked: true,
+  },
+] as const;
 
 //          ╭─────────────────────────────────────────────────────────╮
 //          │                        component                        │
 //          ╰─────────────────────────────────────────────────────────╯
 export const CryptoChart: FC = () => {
+  const [indexes, setIndexes] = useState<IIndexController[]>([...INDEXES]);
+
+  const handleIndexChange = (item: IIndexController) => {
+    setIndexes((prev) => {
+      return prev.map((prevItem) => {
+        if (prevItem.key === item.key) {
+          return item;
+        }
+
+        return prevItem;
+      });
+    });
+  };
+
   return (
     <div className='grid grid-rows-2 w-full m-12 gap-4 p-4 rounded-xl bg-[#f5f6f8]'>
       <div className='grid grid-cols-12 w-full gap-4'>
@@ -23,7 +59,7 @@ export const CryptoChart: FC = () => {
         </div>
       </div>
       <div className='bg-white p-4 shadow-md shadow-gray-200 rounded-xl flex'>
-        <IndexController />
+        <IndexController indexes={indexes} handleIndexChange={handleIndexChange} />
 
         <div className='w-full p-8'>2</div>
         <div className='w-full p-8'>3</div>
