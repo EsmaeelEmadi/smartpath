ce4f7ed8fba2770696e31f9a957e31a39a908fed
emad <emadi68moh@gmail.com>
2024-05-29 03:14:34 +0330

    feat: added indexes props to PriceChart

--- src/lib/components/common/features/Crypto/CryptoChart.tsx
+++ src/lib/components/common/features/Crypto/CryptoChart.tsx
@@ -52,7 +52,7 @@ export const CryptoChart: FC = () => {
     <div className='grid grid-rows-2 w-full m-12 gap-4 p-4 rounded-xl bg-[#f5f6f8]'>
       <div className='grid grid-cols-12 w-full gap-4'>
         <div className='col-span-8 rounded-xl bg-white p-4 shadow-md shadow-gray-200'>
-          <PriceChart />
+          <PriceChart indexes={indexes} />
         </div>
         <div className='col-span-4 rounded-xl bg-white p-4 min-h-96 shadow-md shadow-gray-200'>
           <MarketVolume />
