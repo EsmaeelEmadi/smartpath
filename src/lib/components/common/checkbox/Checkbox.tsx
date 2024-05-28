c3c8d7fe7cd7b5d4d2b8143c4d233fa3ef4b7fb2
emad <emadi68moh@gmail.com>
2024-05-29 01:15:37 +0330

    feat: added color props

--- src/lib/components/common/checkbox/Checkbox.tsx
+++ src/lib/components/common/checkbox/Checkbox.tsx
@@ -2,16 +2,26 @@ import classNames from 'classnames';
 
 // ── types
 import type { FC, InputHTMLAttributes } from 'react';
+import { TColor } from '../../../util/types/theme';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface ICheckboxProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> {}
+interface ICheckboxProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> {
+  color?: TColor;
+}
 
-export const Checkbox: FC<ICheckboxProps> = ({ className, ...rest }) => {
+export const Checkbox: FC<ICheckboxProps> = ({ className, color = 'success', ...rest }) => {
   return (
     <input
       type='checkbox'
       className={classNames(
-        'appearance-none bg-transparent accent-custom-green-500 border-custom-green-400 border-2 rounded-full w-4 h-4 checked:bg-green-400 transition-all duration-100 outline-none cursor-pointer',
+        'appearance-none bg-transparent border-2 rounded-full w-4 h-4 transition-all duration-100 outline-none cursor-pointer',
+        {
+          'accent-custom-green-500 border-custom-green-500 checked:bg-custom-green-500':
+            color === 'success',
+          'accent-custom-golden-500 border-custom-golden-500 checked:bg-custom-golden-500':
+            color === 'warning',
+          'accent-custom-red-500 border-custom-red-500 checked:bg-custom-ren-500':
+            color === 'danger',
+        },
         className,
       )}
       {...rest}
